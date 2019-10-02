7739524e7b6977ec85aaf10b866344ed72c3785e
cokpsz <barjuandavis@gmail.com>
2019-10-02 14:07:08 +0700

    2/10/2019

--- app/src/main/java/com/wesync/MetronomeService.kt
+++ app/src/main/java/com/wesync/MetronomeService.kt
@@ -1,12 +1,17 @@
 package com.wesync
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
+import android.os.Vibrator
 
 object MetronomeService: Service(), Runnable {
+    //TODO: make LiveData for configuration changes
     private lateinit var binder: IBinder
-    
+    private lateinit var vibrator: Vibrator
+
 
     override fun onBind(p0: Intent?): IBinder? {
         return binder;
@@ -14,6 +19,21 @@ object MetronomeService: Service(), Runnable {
 
     override fun run() {
 
+    }
+
+    private fun playSound() {
+        //TODO: check if sound is available in the config
+        val mp = MediaPlayer.create(this,R.raw.tick)
+        mp.start()
+        mp.setOnCompletionListener { mp.release() }
+    }
+
+    private fun playVibrate() {
+        //TODO: check if vibrate is available in the config
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrator.vibrate()
+
+        
     }
 
     private var bpm: Int = 120
