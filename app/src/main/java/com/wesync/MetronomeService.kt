1ba840307c6f813add3078fd2a5212c6eb8531b8
cokpsz <barjuandavis@gmail.com>
2019-10-02 15:24:15 +0700

    2/10/2019

--- app/src/main/java/com/wesync/MetronomeService.kt
+++ app/src/main/java/com/wesync/MetronomeService.kt
@@ -4,20 +4,28 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.os.Vibrator
 
 object MetronomeService: Service(), Runnable {
     //TODO: make LiveData for configuration changes
-    private lateinit var binder: IBinder
+    private val binder = LocalBinder()
     private lateinit var vibrator: Vibrator
 
+    private class LocalBinder : Binder() {
+        fun getService() : MetronomeService {
+            return MetronomeService
+        }
+    }
 
     override fun onBind(p0: Intent?): IBinder? {
-        return binder;
+        return binder
     }
 
     override fun run() {
+        //run this service on the background according to the config
+
 
     }
 
@@ -31,9 +39,11 @@ object MetronomeService: Service(), Runnable {
     private fun playVibrate() {
         //TODO: check if vibrate is available in the config
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        vibrator.vibrate()
+        vibrator.vibrate(500)
+    }
 
-        
+    private fun playFlashScreen() {
+        //TODO: find out how to communicate to the UI thread safely (and with the least latency possible)
     }
 
     private var bpm: Int = 120
