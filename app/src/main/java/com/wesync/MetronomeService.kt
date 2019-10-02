cb661dda153caa37c1f8003b361ca6a14c9b5998
cokpsz <barjuandavis@gmail.com>
2019-10-02 11:40:48 +0700

    2/10/2019

--- app/src/main/java/com/wesync/MetronomeService.kt
+++ app/src/main/java/com/wesync/MetronomeService.kt
@@ -4,13 +4,12 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 
-class MetronomeService: Service(), Runnable {
+object MetronomeService: Service(), Runnable {
     private lateinit var binder: IBinder
-
-
+    
 
     override fun onBind(p0: Intent?): IBinder? {
-
+        return binder;
     }
 
     override fun run() {
