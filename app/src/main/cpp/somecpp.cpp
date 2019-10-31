b1811641320321cb1208cde723a219076f9c62ea
Barjuan Davis <barjuandavis@gmail.com>
2019-10-31 14:36:10 +0700

    30/10/2019

---
+++ app/src/main/cpp/somecpp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lab on 10/31/2019.
+//
+
+#include "somecpp.h"
---
+++ app/src/main/cpp/somecpp.h
@@ -0,0 +1,16 @@
+//
+// Created by lab on 10/31/2019.
+//
+
+#ifndef WESYNC_SOMECPP_H
+#define WESYNC_SOMECPP_H
+
+
+
+class somecpp {
+
+};
+
+
+
+#endif //WESYNC_SOMECPP_H
